578c541138e3e39b1381526dc71f48085630fb07
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-05-22 19:55:56 +0530

    added json server and fetched api

--- react-blog/src/BlogList.js
+++ react-blog/src/BlogList.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const BlogList = ({bloglist, titlehead, handleDelete}) => {
+const BlogList = ({bloglist, titlehead,}) => {
     const blogsup = bloglist;
     const title = titlehead;
     return (
@@ -10,7 +10,6 @@ const BlogList = ({bloglist, titlehead, handleDelete}) => {
                  <div className="blog-preview" key={blog.id}>
                      <h2>{blog.title}</h2>
                      <p>The blog is written by {blog.body}</p>
-                     <button onClick ={()=> handleDelete(blog.id)}>Delete Blog</button>
                  </div>
              ))}
         </div>
--- react-blog/src/Home.js
+++ react-blog/src/Home.js
@@ -4,29 +4,28 @@ import BlogList from './BlogList'
 
 const Home = () => {
 
-const [blogs, setBlogs] = useState([
-    {title: 'My new Website', body: 'lorem ipsum...', author: 'mario', id: 1},
-    {title: 'My new Party', body: 'lorem ipsum...', author: 'luigi', id: 2},
-    {title: 'My new Disco', body: 'lorem ipsum...', author: 'princess', id: 3}
-]);
-
-const [name, setName] = useState('mario')
+const [blogs, setBlogs] = useState(null);
 
 
-const handleDelete=(id)=>{
-    const newBlogs = blogs.filter((blog)=> blog.id !== id );
-    setBlogs(newBlogs);
-}
+
+// const handleDelete=(id)=>{
+//     const newBlogs = blogs.filter((blog)=> blog.id !== id );
+//     setBlogs(newBlogs);
+// }
 
 useEffect(()=>{
-    console.log('hello')
+    fetch('http://localhost:8000/blogs')
+    .then(res =>{
+        return res.json();
+    })
+    .then(blogData =>{
+        setBlogs(blogData);
+    })
 }, []);
 
     return (
         <div className="home">
-             <BlogList bloglist = {blogs} titlehead = 'All Blogs!' handleDelete = {handleDelete} home = {Home} />
-            <p>{name}</p>
-            <button onClick = {()=>setName('luigi')}>Change Name</button>
+             {blogs && <BlogList bloglist = {blogs} titlehead = 'All Blogs!' />}
         </div>
     )
 }
