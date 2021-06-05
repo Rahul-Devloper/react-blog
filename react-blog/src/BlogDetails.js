10bcc2fb739f77a4155ae148f8d6a26f05be6f44
Rahul-Devloper <rahulramesh26@yahoo.com>
2021-06-05 12:35:20 +0530

    added delete requests

--- react-blog/src/BlogDetails.js
+++ react-blog/src/BlogDetails.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { useParams } from 'react-router';
+import { useHistory, useParams } from 'react-router';
 import useFetch from './useFetch'
 
 const BlogDetails = () => {
 
     const {id} = useParams();
     const {data: blog, error, isLoading} = useFetch('http://localhost:8000/blogs/' + id);
-
+    const history = useHistory();
+    const handleCLick = ()=>{
+        fetch('http://localhost:8000/blogs/' + blog.id, {
+            method: 'DELETE'
+        }).then(()=>{
+            history.push('/');
+        })
+    }
     return (
         <div className='blog-details'>
             {isLoading && <div>Loading...</div>}
@@ -16,6 +23,7 @@ const BlogDetails = () => {
                     <h2>{blog.details}</h2>
                     <p>Written By {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleCLick}>delete</button>
                 </article>
             )}
         </div>
--- react-blog/src/Create.js
+++ react-blog/src/Create.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react'
+import {useHistory} from 'react-router-dom'
 
 const Create = () => {
 
     const [title, setTitle] = useState('');
     const [body, setBody] = useState('');
     const [author, setAuthor] = useState('mario');
+    const [isLoading, setIsLoading] = useState(false);
+    const history = useHistory();
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const blog={title, body, author}
+        setIsLoading(true);
+       fetch('http://localhost:8000/blogs',{
+           method:'POST',
+           headers: {'Content-Type': 'application/json'},
+           body: JSON.stringify(blog)
+       }).then(()=>{
+           alert('New Blog Added');
+           setIsLoading(false);
+            history.push('/');
+       })
+    }
 
     return (
         <div className= 'create'>
            <h2>Add a New Blog</h2> 
-           <form>
+           <form onSubmit={handleSubmit}>
                <label>Blog Title</label>
                <input 
                type="text" 
@@ -31,10 +49,8 @@ const Create = () => {
                    <option value="mario">Mario</option>
                    <option value="yoshi">Yoshi</option>
                </select>
-               <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+               {!isLoading && <button>Add Blog</button>}
+               {isLoading && <button disabled>Adding Blog...</button>}
            </form>
         </div>
     )
